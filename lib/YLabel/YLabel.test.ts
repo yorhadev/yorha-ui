0ea013e84f02196ea3abe064553f5d8ca9191e47
yorhadev <rafael.takano.dev@gmail.com>
2023-01-05 16:12:03 -0300

    feat: parcial - creating y-label component

---
+++ lib/YLabel/YLabel.test.ts
@@ -0,0 +1,8 @@
+import { mount } from "@vue/test-utils";
+import { test, expect } from "vitest";
+import YLabel from "./YLabel.vue";
+
+test("it renders a label", async () => {
+  const YLabelComponent = mount(YLabel);
+  expect(YLabelComponent.find("input").element).toBeTruthy();
+});
